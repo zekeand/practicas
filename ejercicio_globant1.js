9d5b8d1ca7ec20503fd0a237ad8c68613d805826
Andres Cuervo <77127249+zekeand@users.noreply.github.com>
2024-01-12 02:57:41 +0000

    initial

---
+++ ejercicio_globant1.js
@@ -0,0 +1,45 @@
+function calc_change(numeroinc){
+let numeros={
+    "-1": [0, 0, 0, 0, 0, 0, 0, 0],
+    "0": [1, 1, 1, 1, 1, 1, 1, 0],
+    "1": [0, 1, 1, 0, 0, 0, 0, 0],
+    "2": [1, 1, 0, 1, 1, 0, 1, 0],
+    "3": [1, 1, 1, 1, 0, 0, 1, 0],
+    "4": [0, 1, 1, 0, 0, 1, 1, 0],
+    "5": [1, 0, 1, 1, 0, 1, 1, 0],
+    "6": [1, 0, 1, 1, 1, 1, 1, 0],
+    "7": [1, 1, 1, 0, 0, 0, 0, 0],
+    "8": [1, 1, 1, 1, 1, 1, 1, 0],
+    "9": [1, 1, 1, 1, 0, 1, 1, 0]
+}
+//let numeroinc = '15'
+let numeroCambios= numeroinc.length +1
+let resultado= [];
+//console.log(numeros)
+function calcularcambio(arr1, arr2){
+    let numcambios = 0;
+    for (let i =0; i< arr1.length ; i++){
+        if (arr1[i] !== arr2[i]){
+            numcambios++
+        }
+    }
+    return numcambios
+}
+let digitoOld
+for (let j=0;j<numeroinc.length;j++){
+    let digito=numeroinc[j]
+    
+    //console.log(digito)
+    if (j === 0){
+        resultado[j]=calcularcambio(numeros['-1'], numeros[digito])
+        digitoOld=digito;
+    } else{
+        resultado[j]=calcularcambio(numeros[digitoOld], numeros[digito])
+    }
+}
+
+//resultado = calcularcambio(numeros['0'],numeros['1'])
+resultado = resultado.reduce((accum,current) => accum+current)
+console.log(resultado)
+}
+calc_change('123')
