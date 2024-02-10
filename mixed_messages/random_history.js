c7ad8b5e44cb7704b24bf53355d95748b2a4dc19
Andres Cuervo <77127249+zekeand@users.noreply.github.com>
2024-02-10 23:24:32 +0000

    final work

--- mixed_messages/random_history.js
+++ mixed_messages/random_history.js
@@ -1,8 +1,6 @@
 const wowProfessions = ["Alchemist", "Blacksmither", "Enchanter", "Engineer", "Herbalist", "Inscriptioneer", "Jewelcrafter", "Leatherworker", "Miner", "Skinner", "Tailored", "Archaeologhist", "Chef", "Fisher", "First Aider"];
 
-  const wowSyllables = ["ar", "an", "as", "ath", "al", "ad", "ag", "am", "az", "ak", "er", "en", "el", "eth", "ed", "eg", "em", "ez", "ek", "ir", "in", "il", "ith", "id", "ig", "im", "iz", "ik", "or", "on", "ol", "oth", "od", "og", "om", "oz", "ok", "ur", "un", "ul", "uth", "ud", "ug", "um", "uz", "uk", "ba", "be", "bi", "bo", "bu", "da", "de", "di", "do", "du",
-    "ga", "ge", "gi", "go", "gu", "ka", "ke", "ki", "ko", "ku", "la", "le", "li", "lo", "lu", "ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu", "pa", "pe", "pi", "po", "pu",
-    "ra", "re", "ri", "ro", "ru", "sa", "se", "si", "so", "su", "ta", "te", "ti", "to", "tu", "va", "ve", "vi", "vo", "vu", "za", "ze", "zi", "zo", "zu"];
+  const wowSyllables = ["ar", "an", "as", "ath", "al", "ad", "ag", "am", "az", "ak", "er", "en", "el", "eth", "ed", "eg", "em", "ez", "ek", "ir", "in", "il", "ith", "id", "ig", "im", "iz", "ik", "or", "on", "ol", "oth", "od", "og", "om", "oz", "ok", "ur", "un", "ul", "uth", "ud", "ug", "um", "uz", "uk", "ba", "be", "bi", "bo", "bu", "da", "de", "di", "do", "du", "ga", "ge", "gi", "go", "gu", "ka", "ke", "ki", "ko", "ku", "la", "le", "li", "lo", "lu", "ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu", "pa", "pe", "pi", "po", "pu", "ra", "re", "ri", "ro", "ru", "sa", "se", "si", "so", "su", "ta", "te", "ti", "to", "tu", "va", "ve", "vi", "vo", "vu", "za", "ze", "zi", "zo", "zu"];
 
   const wowTownNames = ["Stormholm", "Ironhaven", "Goldshire", "Silverpeak", "Shadowglen", "Frostwatch", "Sunset Ridge", "Moonfall", "Stonewatch", "Thunderbluff", "Starfall",   "Eaglespire", "Hearthglen", "Wildhammer", "Ravenholdt", "Havenwood", "Dragonmire", "Sunspear", "Shadowvale", "Duskwood", "Winter's Edge", "Firecrest", "Deepforge", "Light's Hope", "Greenhaven", "Brightwater", "Silverwood", "Mistvale", "Highglen", "Lion's Arch", "Trollbane", "Thunderkeep", "Stonehaven", "Frostfang", "Riverbend", "Gloomwood", "Thornwood",
     "Stormshield", "Ironforge", "Goldcrest", "Silverwood", "Shadowfen", "Frosthold", "Sunrise Valley", "Moonhaven", "Stonewall",  "Thunderwatch",    "Starglade",    "Eaglesnest",  "Hearthstone",  "Wildwood", "Ravenwood","Havencrest", "Dragonspire", "Sunvale", "Shadowfall", "Duskmire",    "Winterhold",  "Firewatch",    "Deephaven",    "Lightspire",
@@ -35,11 +33,23 @@ function random_place(){
 
 
 function random_background(){
+  const name1=random_name()
+  const profession = random_profession()
+  const place=random_place()
+  const texto_desc=document.getElementById("gen_descrp")
+  texto_desc.textContent = `let\'s meet ${name1}, a ${profession} by profession. Born and rise in ${place}.`
+  
+  //obtener el estado actual de la propiedad hidden
+  const hiddenState = texto_desc.getAttribute('hidden');
 
+  // Cambiar el estado de 'hidden' para mostrar u ocultar el texto
+  if (hiddenState === 'true') {
+    texto_desc.removeAttribute('hidden');
+  }
 }
+const boton = document.getElementById("bn_gen")
+boton.addEventListener('click', random_background);
 
-let name1=random_name()
-let profession = random_profession()
-let place=random_place()
-console.log(`let\'s meet ${name1}, a ${profession} by profession. Born and rise in ${place}.`)
+
+//console.log(`let\'s meet ${name1}, a ${profession} by profession. Born and rise in ${place}.`)
 
